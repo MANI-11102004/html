735f3a6cfdec69c651dd2aa6e1deb5994bfc539e
MANI-11102004 <manishankar.papana@gmail.com>
2024-03-07 19:08:32 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log("mani")
+console.log(`manishankar`)
+document.getElementById("h1").textContent="mani"
+document.getElementById(`p`).textContent=`paragraph`
+let m=123654
+console.log(m)
+console.log(typeof m)
+let f=1.25
+console.log(f)
+console.log(typeof f)
+let s="mani shankar"
+console.log(s)
+console.log(typeof s)
+console.log(`your number is ${m}`)
+console.log(`your name is ${s}`)
+document.getElementsByClassName("h").textContent=s
